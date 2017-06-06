68355530e408d93ea2d4a74d448d61120c5489e2
Joel <jijoel@yahoo.com>
2017-06-06 01:06:31 +0000

    split out a master view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,23 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ app()->getLocale() }}" class="environment {{ app()->environment() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
+@section('body')
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/vuetify.min.css') }}" rel="stylesheet">
-    @yield('css')
-</head>
-<body>
-
-<div id="app">
     <va-layout
         name="{{ config('app.name', 'Laravel') }}"
     >
@@ -25,10 +9,5 @@
         @yield('content')
 
     </va-layout>
-</div>
 
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}"></script>
-    @yield('js')
-</body>
-</html>
+@endSection
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}" class="environment {{ app()->environment() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/vuetify.min.css') }}" rel="stylesheet">
+    @yield('css')
+</head>
+<body>
+
+    <div id="app">
+        @yield('body')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ mix('js/app.js') }}"></script>
+    @yield('js')
+</body>
+</html>
