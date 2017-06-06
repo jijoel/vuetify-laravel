2e009860cd156478be99dcf322b7b2c396aea4c6
Joel <jijoel@yahoo.com>
2017-06-06 02:35:21 +0000

    link to login and register pages

--- resources/assets/js/components/Layout.vue
+++ resources/assets/js/components/Layout.vue
@@ -38,7 +38,7 @@
 
     <v-toolbar fixed class="indigo darken-4" light>
       <v-toolbar-side-icon light @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Toolbar</v-toolbar-title>
+      <v-toolbar-title>{{ name }}</v-toolbar-title>
     </v-toolbar>
     <main>
       <v-container fluid>
