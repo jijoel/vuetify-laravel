7dd0b24c3e88457630d83b7131f37e988b03b00f
Joel <jijoel@yahoo.com>
2017-06-06 05:33:29 +0000

    render cards under carousel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,36 @@
   </v-carousel-item>
 </v-carousel>
 
+<v-container fluid>
+    <v-layout row wrap justify-center>
+
+    <v-card
+      v-for="(item,i) in carousel"
+      height="300px"
+      style="width:400px"
+      class="ma-1"
+    >
+      <v-card-row class="primary" height="60px">
+        <v-card-title class="white--text">
+          @{{ item.title }}
+        </v-card-title>
+      </v-card-row>
+      <v-card-row :img="item.picture" height="180px">
+        <v-card-text class="white--text">
+          @{{ item.text }}
+        </v-card-text>
+      </v-card-row>
+      <v-card-row actions height="60px">
+        <v-spacer></v-spacer>
+        <v-btn primary light>Do Something</v-btn>
+        <v-spacer></v-spacer>
+      </v-card-row>
+    </v-card>
+
+
+  </v-layout>
+</v-container>
+
 @endSection
 
 
@@ -36,27 +66,24 @@
         carousel: [
           {
             title: 'Squirrel!!!',
-            picture: '/img/carousel/squirrel.jpg'
+            picture: '/img/carousel/squirrel.jpg',
+            text: 'Itaque tenetur nemo nobis quidem dolores provident quaerat ut veritatis, repellat quam, fugiat perferendis soluta excepturi consectetur eos cum aliquid cumque facilis.',
           },
           {
             title: 'Second',
-            picture: '/img/carousel/sky.jpg'
+            picture: '/img/carousel/sky.jpg',
+            text: 'Quae fugit rem, dignissimos voluptatem praesentium impedit officiis laudantium voluptatum nobis, aliquid quos temporibus, eaque. Ducimus ipsa quo, maiores sequi similique quaerat. Accusantium quia, dolores. Adipisci eligendi, nobis nisi architecto a dolorem quod animi in est quae modi sint dolores quasi doloribus aut fugiat.',
           },
           {
             title: 'Third',
-            picture: '/img/carousel/bird.jpg'
+            picture: '/img/carousel/bird.jpg',
+            text: 'Soluta, eaque repellendus, possimus consectetur quia ipsum voluptate, nam voluptates nostrum repellat commodi aliquid eos harum deleniti rerum! Asperiores culpa fuga, id.',
           },
           {
             title: '',
-            picture: '/img/carousel/planet.jpg'
+            picture: '/img/carousel/planet.jpg',
+            text: 'Aliquam rerum, voluptatum doloribus voluptatem aspernatur voluptates?',
           },
-        ],
-
-        cards: [
-          {
-            'title': 'Foo',
-            'body': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet adipisci, nemo ipsa odio velit, cum, iure commodi modi fugit corporis dolore, maiores voluptatibus perspiciatis quod magnam impedit qui ex in!',
-          }
         ],
     };
 </script>
