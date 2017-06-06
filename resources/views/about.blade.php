71e0cd02ead8eac40cfdfec7a6ba421620ffdd8e
Joel <jijoel@yahoo.com>
2017-06-06 00:30:04 +0000

    create about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1 class="display-3">About</h1>
+
+    <p>This is a project to learn some of the basics of using <a href="https://laravel.com">Laravel</a> with <a href="https://vuejs.org/">Vue</a> and <a href="https://vuetifyjs.com/">Vuetify</a>.</p>
+
+@endSection
