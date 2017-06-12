2eb9612dbeb4bf93b1db70049f1947fb774171da
Joel <jijoel@yahoo.com>
2017-06-12 10:41:15 -1000

    create Email control

---
+++ resources/assets/js/controls/Email.vue
@@ -0,0 +1,26 @@
+<script>
+import TextField from 'vuetify/src/components/forms/TextField.js';
+
+export default {
+
+  name: 'email',
+
+  mixins: [ TextField ],
+
+  props: {
+    'label': {
+      accept: String,
+      default: 'Email Address',
+    },
+    'type': {
+      accept: String,
+      default: 'email',
+    },
+    'prepend-icon': {
+      accept: String,
+      default: 'mail',
+    },
+  },
+
+}
+</script>
